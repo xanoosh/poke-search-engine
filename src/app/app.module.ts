0314074e5d7973b7f2032de5faebce7fe0f2bf4b
xanoosh <xanoosh@gmail.com>
2022-04-03 11:02:53 +0200

    added header and searchbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
