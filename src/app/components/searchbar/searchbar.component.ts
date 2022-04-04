76c4684295b52fae258bb17cc447e785a49bd59b
xanoosh <xanoosh@gmail.com>
2022-04-04 15:52:19 +0200

    working on an api call

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
   styleUrls: ['./searchbar.component.scss'],
 })
 export class SearchbarComponent implements OnInit {
-  searchBarChange: (val: any) => void = (val) => console.log(val);
+  searchBarValue: string = '';
+  searchBarChange: (val: any) => void = (val) => {
+    this.searchBarValue = val;
+    console.log(this.searchBarValue);
+  };
   searchBarClick: () => void = () => console.log('clicked');
-
-  constructor() {}
-
+  data: any;
+  constructor(private http: HttpClient) {}
+  getPokemonData() {
+    this.data = this.http.get(
+      `https://pokeapi.co/api/v2/pokemon/${this.searchBarValue}`
+    );
+    console.log(this.data);
+  }
   ngOnInit(): void {}
 }
